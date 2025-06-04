876896c141b42c683fa6ec506cfd08b96ef6dbdd
Mkien <manhkien1005@gmail.com>
2025-06-04 17:48:31 +0700

    4

--- chatbot.py
+++ chatbot.py
@@ -18,7 +18,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from jose import JWTError, jwt
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.encoders import jsonable_encoder
-import rarfile
+import gdown
 # Load biến môi trường từ file .env
 load_dotenv()
 MONGO_URL = os.getenv("MONGODB_URI")
@@ -32,30 +32,26 @@ app = FastAPI()
 # Cấu hình CORS
 origins = os.getenv("ORIGINS").split(",")
 def download_chroma_from_drive():
-    CHROMA_PATH = "chroma_db"
-    GOOGLE_DRIVE_FILE_ID = "1syQQfIsD0lzrUhFDC5E_Gf7gC1Ubgllm"
-    CHROMA_ZIP = "chroma_db.rar"
+    CHROMA_PATH = "chroma_db1"
+    GOOGLE_DRIVE_FILE_ID = "1VcLJ-KZhsQLbMTcDKFTQioj-dRU_6hP4"
+    CHROMA_ZIP = "chroma_db.zip"
     if os.path.exists(CHROMA_PATH):
         print("[INFO] ChromaDB đã tồn tại.")
         # xóa folder chroma_db
         shutil.rmtree(CHROMA_PATH)
-
     print("[INFO] Tải ChromaDB từ Google Drive...")
     url = f"https://drive.google.com/uc?export=download&id={GOOGLE_DRIVE_FILE_ID}"
-    response = requests.get(url, stream=True)
+    # response = requests.get(url)
+    # print(response)
+    gdown.download(f"https://drive.google.com/uc?id={GOOGLE_DRIVE_FILE_ID}", output="chroma_db.zip", quiet=False)
+    # Giải nén file ZIP
+    print("[INFO] Đang giải nén...")
+    with zipfile.ZipFile(CHROMA_ZIP, 'r') as zip_ref:
+        zip_ref.extractall(CHROMA_PATH)
+    print("[INFO] Giải nén hoàn tất!")
 
-    with open(CHROMA_ZIP, "wb") as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
-
-    print("[INFO] Giải nén ChromaDB vào thư mục chroma_db/...")
-    os.makedirs(CHROMA_PATH, exist_ok=True)
-    with rarfile.RarFile(CHROMA_ZIP, 'r') as rar_ref:
-        rar_ref.extractall(CHROMA_PATH)
-
+    # Xóa file zip nếu muốn
     os.remove(CHROMA_ZIP)
-    print("[INFO] Hoàn tất.")
-
 download_chroma_from_drive()
 # Khởi tạo vector store
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
